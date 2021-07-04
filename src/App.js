971f055704dc7119f542a70372540f836cb2519f
Erika Espejo <erikaespejo95@gmail.com>
2021-07-04 12:16:21 -0500

    increment feature

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    valor: 0,
+  };
+
+  increment = () => {
+    this.setState((prevState) => {
+      return { valor: prevState.valor + 1 };
+    });
+  };
+
   render() {
+    const { valor } = this.state;
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{valor}</span>
+        <button id="inc" onClick={this.increment}>
+          Incrementa
+        </button>
       </div>
     );
   }
